e7953f7ce6bec70c66e3d186ee6b32fef6c4b905
C-rostad <Carter@rostad.ca>
2025-05-17 13:41:10 -0700

    Implemented tail function for arrays

---
+++ tail.js
@@ -0,0 +1,31 @@
+const tail = function(inputArray) {
+  const returnArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (i > 0) {
+      returnArray.push(inputArray[i]);
+    }
+  }
+  return returnArray;
+};
+
+console.log(tail([5,6,7,8,9,1])); //test tail function
+console.log(tail(["Testing one element in array"])); // test one element
+console.log(tail([])); // test empty array
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
