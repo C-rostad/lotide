6b8d107069d6c56362e989fb665d2bdbc6b71659
C-rostad <Carter@rostad.ca>
2025-05-17 16:48:42 -0700

    implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2 [i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    return console.log("The arrays are equal");
+  } else {
+    return console.log("The arrays are not equal");
+  }
+ 
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);// true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // false
