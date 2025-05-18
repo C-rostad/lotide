5d3ee01b7727e67e565ae26346f2513f0de5c7bb
C-rostad <Carter@rostad.ca>
2025-05-17 17:26:22 -0700

    implemented flatten

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2 [i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    return console.log("The arrays are equal");
+  } else {
+    return console.log("The arrays are not equal");
+  }
+ 
+};
+
+const flatten = function(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        returnArray.push(array[i][j]);
+      }
+    } else {
+      returnArray.push(array[i]);
+    }
+
+  }
+  return returnArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
