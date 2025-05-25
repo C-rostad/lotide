1a668a7d4c49b45275cd5a0047c7b6d349bdbfe8
C-rostad <Carter@rostad.ca>
2025-05-25 15:28:24 -0700

    Working on test cases

--- eqObjects.js
+++ eqObjects.js
@@ -7,17 +7,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+//eqArrays for checking values when object has an array
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
   let object1Keys = Object.keys(object1);
-  object1Keys.sort();
   let object2Keys = Object.keys(object2);
-  object2Keys.sort();
   let object1Values = Object.values(object1);
-  object1Values.sort();
   let object2Values = Object.values(object2);
-  object2Values.sort();
+    object1Keys.sort();
+    object2Keys.sort();
+    object1Values.sort();
+    object2Values.sort();
 
   //check if keys are equal length
   if (object1Keys.length !== object2Keys.length) {
@@ -25,7 +38,22 @@ const eqObjects = function (object1, object2) {
   }
   //check if keys and values are the same
   for (let i = 0; i < object1Keys.length; i++) {
+    if(Array.isArray(object1Values[i]) && Array.isArray(object2Values[i])) { //check if both object values are arrays
+      if (object1Values[i].length !== object2Values[i].length) {
+        return false
+      } else if (eqArrays(object1Values[i], object2Values[i]) === false) { // refactor into || if statement when working
+        return false
+      }
+
+    } else if (Array.isArray(object1Values[i]) || Array.isArray(object2Values[i])) { // if only one of the object values are arrays, return false
+      return false
+    }
+
     if (object1Keys[i] !== object2Keys[i] || object1Values[i] !== object2Values[i]) {
+      // console.log(object1Keys[i]);
+      // console.log(object2Keys[i]);
+      // console.log(object1Values[i]);
+      // console.log(object2Values[i]);
       return false;
     }
   }
@@ -53,8 +81,8 @@ const longSleeveShirt = {
   sleevelength: "long",
 };
 
-eqObjects(shirt, longSleeveShirt); // false
-assertEqual(eqObjects(shirt, longSleeveShirt), false);
+ eqObjects(shirt, longSleeveShirt); // false
+ assertEqual(eqObjects(shirt, longSleeveShirt), false);
 
 const multiColorShirtObject = {
    colors: ["red", "blue"], 
@@ -66,7 +94,9 @@ const anotherMultiColorShirtObject = {
   colors: ["red", "blue"] 
 };
 
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // true
+console.log("testbreak")
+console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)); // true
+console.log("testbreak2");
 assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = {
@@ -74,6 +104,6 @@ const longSleeveMultiColorShirtObject = {
   colors: ["red", "blue"],
   sleeveLength: "long",
 };
-
+console.log(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject));
 eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject);; // false
 assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
