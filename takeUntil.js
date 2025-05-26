9f559ccb79cf65d99ba8755a9a3696545f361ec2
C-rostad <Carter@rostad.ca>
2025-05-25 18:03:02 -0700

    first implementation of takeUntil, week 2

---
+++ takeUntil.js
@@ -0,0 +1,63 @@
+//Implement takeUntil, Week 2
+
+const takeUntil = function(array, callback) {
+  const returnArray = [];
+  for (const element of array) {
+    if (callback(element)) {
+      return returnArray;
+    } else {
+      returnArray.push(element);
+    }
+  }
+
+
+
+  return returnArray;
+};
+
+
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2 [i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    return console.log("The arrays are equal");
+  } else {
+    return console.log("The arrays are not equal");
+  }
+ 
+};
+
+
+
+const results3 = takeUntil([], x => x > 2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]); //true
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]); //true
+assertArraysEqual(results3, []); //true
+
+
+
