c489d404d7312e7a279f0570211b171d21e37962
C-rostad <Carter@rostad.ca>
2025-05-25 18:38:04 -0700

    linted, added comments, changed test case

--- findKey.js
+++ findKey.js
@@ -6,17 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 //implement findKey, Week 2
 
@@ -24,7 +13,7 @@ const findKey = function(object, callback) {
   let objectKeys = Object.keys(object);
   let returnKey;
   for (const keys of objectKeys) {
-    if (callback(object[keys])) {
+    if (callback(object[keys])) { //check if callback function has a truthy, if so return the first key to do so
       return keys;
     }
   }
@@ -33,22 +22,21 @@ const findKey = function(object, callback) {
 
 
   return returnKey;
-}
+};
 
 
 const testObject = {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  };
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
 
 const testCallback = (x) => x.stars === 2;
 
 console.log(findKey(testObject, testCallback));
 
   
-
-assertEqual(findKey((testObject, testCallback), "noma"));
+assertEqual(findKey(testObject, testCallback), "noma");
