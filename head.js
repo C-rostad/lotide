6859fce202044e54056e10262e31bce18c1d551d
C-rostad <Carter@rostad.ca>
2025-06-07 19:59:15 -0700

    linted and refactored

--- head.js
+++ head.js
@@ -1,12 +1,8 @@
 const head =  function(inputArray) {
   let returnValue;
-    if (inputArray) {
-      returnValue = inputArray[0];
-    }
-  
-  console.log(returnValue);
+  if (inputArray) {
+    returnValue = inputArray[0];
+  }
   return returnValue;
 };
-
-
 module.exports = head;
