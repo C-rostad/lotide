4109e937b08e2cfcd406698b1b6275a90a9ac516
C-rostad <Carter@rostad.ca>
2025-06-07 20:59:07 -0700

    refactored and added middleTest

--- middle.js
+++ middle.js
@@ -1,37 +1,20 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || typeof array1[i] !== typeof array2 [i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    return console.log("The arrays are equal");
-  } else {
-    return console.log("The arrays are not equal");
-  }
- 
-};
 
 
 const middle = function(array) {
-  let returnArray = [];
+  const returnArray = [];
   if (array.length <= 2) {
     return returnArray;
   }
   if (array.length % 2 === 1) {
-    let middle = (array.length + 1) / 2;
+    const middle = (array.length + 1) / 2;
     returnArray.push(array[middle - 1]);
-  } else {
-    let middle = array.length / 2;
+  } else if (array.length > 1) {
+    const middle = array.length / 2;
     returnArray.push(array[middle - 1]);
     returnArray.push(array[middle]);
   }
   return returnArray;
 };
+
+module.exports = middle;
