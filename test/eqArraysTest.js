c9a60540218228a0d919778b67488cfaf0466b05
C-rostad <Carter@rostad.ca>
2025-06-07 20:28:45 -0700

    changed back to assertEqual for test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,10 @@
 const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertEqual");
+const assertEqual = require("../assertEqual");
 
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // false
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // false
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
