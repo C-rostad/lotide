5199deba8d2a729919336aea534cb7fff2601959
C-rostad <Carter@rostad.ca>
2025-06-07 20:10:56 -0700

    added assert arrays equal to tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,6 @@
 const tail = require("../tail")
 const assertEqual = require("../assertEqual");
+const assertArraysEqual = require("../assertArraysEqual");
 
 
 
@@ -7,7 +8,7 @@ console.log(tail([5,6,7,8,9,1])); //test tail function
 console.log(tail(["Testing one element in array"])); // test one element
 console.log(tail([])); // test empty array
 const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+assertArraysEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
